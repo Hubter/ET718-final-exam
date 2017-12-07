464dfc7b78c3617f0700c8ad7cd47c6ab9a16395
Hubter <zzzzzzz347@gmail.com>
2017-12-07 14:03:30 -0500

    moviedat

---
+++ moviedat/db/migrate/20171207185033_create_movies.rb
@@ -0,0 +1,18 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :cast
+      t.text :description
+      t.string :movieurl
+      t.string :movieposter
+      t.string :posterurl
+      t.text :caption1
+      t.text :caption2
+      t.string :rating
+      t.text :reviews
+
+      t.timestamps
+    end
+  end
+end
